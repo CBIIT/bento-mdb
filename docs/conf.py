877b048d418b2a4371e1de63ad1f7465cb3db693
Nelson Moore <nelson.moore@essential-soft.com>
2023-07-14 17:44:26 -0400

    docs(*): add requirements for readthedocs
    - Add files to enable Read the Docs/Sphinx documentation generation.
    - Move most README.md content to bento-mdb.md.

---
+++ docs/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "bento-mdb"
+copyright = "2023, Mark A. Jensen, Nelson Moore"
+author = "Mark A. Jensen, Nelson Moore"
+release = "0.0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_nb",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
