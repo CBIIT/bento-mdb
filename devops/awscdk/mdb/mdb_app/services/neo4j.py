de2cf86a7fef93ba62e827f0fafe121c27247ba6
Tracy Truong <tracy18@users.noreply.github.com>
2025-07-10 15:46:24 -0400

    modified neo4j plugin dir in the container

--- devops/awscdk/mdb/mdb_app/services/neo4j.py
+++ devops/awscdk/mdb/mdb_app/services/neo4j.py
@@ -32,6 +32,7 @@ class neo4jService:
         "NEO4J_apoc_import_file_enabled":config['db']['apoc_import_file_enabled'],
         "NEO4J_apoc_import_file_use__neo4j__config":config['db']['apoc_file_use_config'],
         "NEO4J_dbms_security_procedures_unrestricted":config['db']['apoc_sec_proc'],
+        "NEO4J_dbms_directories_plugins":config['db']['neo4j_plugin'],
     }
 
     # secrets={
