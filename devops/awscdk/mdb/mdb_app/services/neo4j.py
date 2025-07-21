446624a03d1e28cf2ac48165eb63bc70a506e202
Tracy Truong <tracy18@users.noreply.github.com>
2025-07-21 14:12:47 -0400

    modified the app.py to use default synthesizer

--- devops/awscdk/mdb/mdb_app/services/neo4j.py
+++ devops/awscdk/mdb/mdb_app/services/neo4j.py
@@ -166,7 +166,7 @@ class neo4jService:
         vpc=self.VPC,
         internet_facing=config.getboolean('nlb', 'internet_facing'),
         vpc_subnets=ec2.SubnetSelection(
-            subnets=vpc.select_subnets(one_per_az=True, subnet_type=ec2.SubnetType.PUBLIC_WITH_EGRESS).subnets
+            subnets=self.VPC.select_subnets(one_per_az=True, subnet_type=ec2.SubnetType.PUBLIC_WITH_EGRESS).subnets
         )
     )
     NLBSecurityGroup = ec2.SecurityGroup(self, "NLBSecurityGroup", vpc=self.VPC, allow_all_outbound=True,)
