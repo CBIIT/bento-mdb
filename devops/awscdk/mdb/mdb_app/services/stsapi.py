ddf1ed1e243dd69d4cfea66e770b86a71ca7c23a
Tracy Truong <tracy18@users.noreply.github.com>
2025-08-26 15:23:21 -0400

    added neo4j service name

--- devops/awscdk/mdb/mdb_app/services/stsapi.py
+++ devops/awscdk/mdb/mdb_app/services/stsapi.py
@@ -22,7 +22,7 @@ class stsapiService:
 
     bolt_port = config.getint('neo4j', 'bolt_port') 
     environment={
-#        "NEO4J_MDB_URI":f"bolt://{self.NLB.load_balancer_dns_name}:{bolt_port}",
+        "NEO4J_MDB_URI":f"bolt://{self.NLB.load_balancer_dns_name}:{bolt_port}",
 #        "NEO4J_MDB_URI":"bolt://{}:{}".format(self.NLB.load_balancer_dns_name, config.getint('neo4j', 'bolt_port)),
         "NEO4J_MDB_USER":config['db']['neo4j_user'],
         "NEO4J_MDB_PASS":config['db']['neo4j_password'],
