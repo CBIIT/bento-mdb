30fd6a05c390d08978088f69524c3eb48a3b03c5
nelsonwmoore <nelsonwmoore@gmail.com>
2025-09-25 08:15:37 -0400

    fix(update_c1_upper): fix deployment names
    - Change deployment names to the ones on the prod-tier worker
    - Add step to export from qa before importing to stage
    - Correct work pool name for update_c1_upper deployment in the prefect yaml

--- src/bento_mdb/flows/update_c1_upper.py
+++ src/bento_mdb/flows/update_c1_upper.py
@@ -7,19 +7,29 @@ from bento_mdb.flows.mdb_s3 import get_current_date
 
 
 @flow(name="update-c1-upper")
-def update_c1_upper_flow(
-    key: str,
-) -> None:
+def update_c1_upper_flow() -> None:
     """Orchestration script to update Cloud One Stage and Prod MDBs from graphml file."""
     logger = get_run_logger()
     logger.info("Running update-c1-upper flow...")
+    logger.info("Exporting from cloud-one-mdb-qa to cloudone-mdb-data bucket")
+    run_deployment(
+        name="mdb-export-s3/mdb-export-s3-prod",
+        parameters={
+            "mdb_id": "cloud-one-mdb-qa",
+            "bucket": "cloudone-mdb-data",
+        },
+        timeout=None,
+        as_subflow=True,
+    )
+    current_date = get_current_date()
+    c1_qa_key = f"{current_date}__cloud-one-mdb-qa.graphml"
     logger.info("Importing to cloud-one-mdb-stage from cloudone-mdb-data bucket")
     run_deployment(
-        name="mdb-import-s3/mdb-import-s3",
+        name="mdb-import-s3/mdb-import-s3-prod",
         parameters={
             "mdb_id": "cloud-one-mdb-stage",
             "bucket": "cloudone-mdb-data",
-            "key": key,
+            "key": c1_qa_key,
             "clear_db": True,
         },
         timeout=None,
@@ -27,7 +37,7 @@ def update_c1_upper_flow(
     )
     logger.info("Pruning prerelease data from cloud-one-mdb-stage")
     run_deployment(
-        name="mdb-prune-prerelease/prune-prerelease",
+        name="mdb-prune-prerelease/prune-prerelease-prod",
         parameters={
             "mdb_id": "cloud-one-mdb-stage",
             "dry_run": False,
@@ -37,7 +47,7 @@ def update_c1_upper_flow(
     )
     logger.info("Exporting from cloud-one-mdb-stage to cloudone-mdb-data bucket")
     run_deployment(
-        name="mdb-export-s3/mdb-export-s3",
+        name="mdb-export-s3/mdb-export-s3-prod",
         parameters={
             "mdb_id": "cloud-one-mdb-stage",
             "bucket": "cloudone-mdb-data",
@@ -49,7 +59,7 @@ def update_c1_upper_flow(
     c1_stage_key = f"{current_date}__cloud-one-mdb-stage.graphml"
     logger.info("Importing to cloud-one-mdb-prod from cloudone-mdb-data bucket")
     run_deployment(
-        name="mdb-import-s3/mdb-import-s3",
+        name="mdb-import-s3/mdb-import-s3-prod",
         parameters={
             "mdb_id": "cloud-one-mdb-prod",
             "bucket": "cloudone-mdb-data",
